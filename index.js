7fae7af55ab198f4ac4b7a28b323c5314ddd7eec
tush87 <tusharj8788@gmail.com>
2023-02-08 21:38:47 +0530

    add express-ejs-layouts

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 const express = require('express');
+const expressLayout = require('express-ejs-layouts');
 const port = 8000;
 
 const app = express();
 
 app.set('view engine','ejs');
 app.set('views','./views');
+app.set('layout extractScripts',true);
+app.set('layout extractStyles',true);
 
 app.use(express.urlencoded({extended:false}));
+
+app.use(expressLayout);
 
 app.use('/',require('./routes/index.js'));
 
@@ -15,6 +20,6 @@ app.listen(port,function(err){
         console.log(err);
         return;
     }
-    
+
     console.log(`server is up on port ${port}`);
 })
